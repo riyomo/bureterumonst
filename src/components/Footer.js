a7fd27f612af32e657e8153a82cb3c1f8ab7a17d
riyomo <46554646+riyomo@users.noreply.github.com>
2021-01-30 18:59:08 +0900

    Fotter update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import logo from '../img/logo.jpg'
-import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -14,7 +11,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="モンスト ブレてる運極写真館"
             style={{ width: '14em', height: '10em' }}
           />
         </div>
@@ -22,13 +19,6 @@ const Footer = class extends React.Component {
           <div className="container has-background-black has-text-white-ter">
             <div style={{ maxWidth: '100vw' }} className="columns">
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
                 <a title="twitter" href="https://twitter.com">
                   <img
                     className="fas fa-lg"
@@ -41,13 +31,6 @@ const Footer = class extends React.Component {
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
