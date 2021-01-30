41ab0110a726a7c1f53b0130e4d9240e5ed28f2a
riyomo <46554646+riyomo@users.noreply.github.com>
2021-01-30 18:19:18 +0900

    modoshi

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
       'モンストの運極写真を、わざとブレてる状態でスクリーンショットし公開しているブログです。',
   },
   plugins: [
-    'gatsby-plugin-feed',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
@@ -73,7 +72,6 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    'gatsby-plugin-netlify',
-     // make sure to keep it last in the array
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
